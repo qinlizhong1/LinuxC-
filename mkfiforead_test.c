1ddf1070b019c6e432604be0b7e04d3f633f2f7a
lizhong <13590316596@163.com>
2021-02-21 17:56:07 +0800

    modify  mkfiforead_test.c

--- mkfiforead_test.c
+++ mkfiforead_test.c
@@ -3,12 +3,10 @@
 //
 //gcc -m32 -g -o pthread_create_test pthread_create_test.c -lpthread
 
-/*
- *
- *
- *
- *
- *
+/*命名管道安全问题：
+   只使用一个FIFO文件，如果有多个进程同时向同一个FIFO文件写数据，而只有一个读FIFO进程在同一个FIFO文件中读取数据时，会发生怎么样的情况呢，会发生数据块的相互交错是很正常的？
+   为了解决这一问题，就是让写操作的原子化。怎样才能使写操作原子化呢？答案很简单，系统规定：在一个以O_WRONLY（即阻塞方式）打开的FIFO中， 如果写入的数据长度小于等待PIPE_BUF，
+   那么或者写入全部字节，或者一个字节都不写入。如果所有的写请求都是发往一个阻塞的FIFO的，并且每个写记请求的数据长度小于等于PIPE_BUF字节，系统就可以确保数据决不会交错在一起。
  */
 #include <unistd.h>
 #include <stdlib.h>
@@ -37,7 +35,7 @@ int main(int argc, char **argv)
 
     // 以只读阻塞方式打开管道文件，注意与fifowrite.c文件中的FIFO同名
     pipe_fd = open(fifo_name, open_mode);
-    
+
     // 以只写方式创建保存数据的文件
     data_fd = open("./DataFormFIFO.txt", O_WRONLY | O_CREAT, 0644);
     printf("Process %d result %d\n", getpid(), pipe_fd);
